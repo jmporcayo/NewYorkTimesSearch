07e6da381b16863da7de4ebaa00936a4322c4e1a
JMP <porcayojm@gmail.com>
2018-07-07 13:54:48 -0700

    app.js

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -1,7 +1,20 @@
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-   'api-key': "8acf31a68f074bf0aaf11a1bf83f3ab2",
-   'q': "Sacramento",
-   'begin_date': "20000101",
-   'end_date': "20100101"
-});
+function buildQuery() {
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "8acf31a68f074bf0aaf11a1bf83f3ab2",
+        'q': "Sacramento",
+        'begin_date': "20000101",
+        'end_date': "20100101"
+    });
+
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
+
+
+}
