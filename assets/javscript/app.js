f005e91f757df4e874ea49df1de6604540de17a5
JMP <porcayojm@gmail.com>
2018-07-07 13:38:27 -0700

    first inital commit

---
+++ assets/javscript/app.js
@@ -0,0 +1,7 @@
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+   'api-key': "8acf31a68f074bf0aaf11a1bf83f3ab2",
+   'q': "Sacramento",
+   'begin_date': "20000101",
+   'end_date': "20100101"
+});
